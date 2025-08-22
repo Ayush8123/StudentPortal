812b58f8bf0053ea7ff14a51c3a6c37c78bc0e26
Ayush shah <shahayush091@gmail.com>
2025-08-22 18:00:53 +0530

    render

--- app.py
+++ app.py
@@ -126,3 +126,9 @@ def admin():
     return render_template("admin.html",mystu=all_stu)
 if __name__=="__main__":
     app.run(debug=True)
+
+import os
+
+if __name__ == "__main__":
+    port = int(os.environ.get("PORT", 5000))
+    app.run(host="0.0.0.0", port=port)
